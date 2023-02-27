69bc1c8d811098ee965a0dee618762dbdc5be986
shawn0rion <shawnorion07@gmail.com>
2023-02-26 19:22:47 -0600

    Can calculate number pairs.

--- script.js
+++ script.js
@@ -15,21 +15,59 @@ function divide (a, b){
 }
 
 function operate(a, b){
+    let a = memory[0];
+    let operator = memory[1];
+    let b = memory[2];
     let calc = {
         '+': add(a, b),
         '-': subtract(a, b),
         '*': multiply(a, b),
         '/': divide(a, b),
     };
-    let operator = ""
-    return calc[operator];
+    let result = calc[operator];
+    memory = [];
+    memory.push(result);
+    return result;
+}
+
+function workingMemory(e) {
+    let arg = e.target.innerHTML;
+    
+    // [2] -> = -> [2]
+    if (memory.length != 3 && arg == '='){
+        console.log('invalid input: ' + arg);
+        return '';
+    }
+    if (memory.length < 1 && !(isNaN(arg))){
+        memory.push(arg);
+        return arg;
+    }
+    // [2] -> 2 -> [22]
+    if (!(isNaN(arg)) && !(isNaN((memory[memory.length - 1])))){
+        return memory[memory.length - 1] += arg;
+    }
+    // [22] -> + -> [22, +]
+    if (e.target.className === 'operator' && !(isNaN(memory[memory.length-1]))){
+        memory.push(arg);
+        return arg;
+    }
+    // [2, +] -> 3 -> [2, +, 3]
+    if (isNaN(memory[memory.length - 1]) && !(isNaN(arg))){
+        memory.push(arg);
+        return arg;
+    }
+    if (memory.length == 3 && arg == '='){
+        return operate();
+    }
+    //repeating operators
+    return memory[memory.length - 1];
 }
 
 function toDisplay (e) {
-    display.innerHTML = e.target.innerHTML;
-
+    display.innerHTML = workingMemory(e);
 }
 
+let memory = []
 const digits = document.querySelectorAll('.digit');
 digits.forEach((digit) => digit.addEventListener('click', toDisplay));
 const operators = document.querySelectorAll('.operator');
